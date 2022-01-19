e93bced2499c78e138f8571d787bf0d50d209788
AjiteshMahalingam <69707961+AjiteshMahalingam@users.noreply.github.com>
2022-01-19 11:50:53 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,28 +1,18 @@
-const jwtExp = require('express-jwt');
 const jwt = require('json-web-token');
 const User = require('../models/User');
 
 const auth = async (req, res, next) => {
-    // To retreive jwt from cookie
-    jwtExp({
-        secret: process.env.jwtSecret,
-        algorithms: ['RS256'],
-        getToken: req => req.cookies.token
-    });
-
     try{
-        const decoded = jwt.verify(req.cookies.token, process.env.jwtSecret);
-        var isAuth = false;
-        const user = await User.findById(decoded._id);
-        user.tokens.map(token => {
-            if (token.token === req.cookies.token )
-                isAuth = true;
-        })
-        req.isAuth = isAuth;
-        req.decoded = decoded;
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.jwtSecret);
+        const user = await User.findOne({ _id : decoded._id, 'tokens.token' : token});
+        if(!user)
+            throw new Error();
+        req.user = user;
+        req.token = token;
         next();
     } catch(e) {
-        res.send(e);
+        res.status(401).send({ error : "Please Authenticate. "});
     }
 };
 
