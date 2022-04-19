0e6758ef469588b0619c49224d8dcb2ce70da2bf
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2022-04-19 18:08:09 +0530

    payment

---
+++ src/data/config.js
@@ -0,0 +1,32 @@
+exports.paymentConfig = {
+  16: {
+    purpose: "Workshop - stock-o-nomics",
+    amount: 149.0,
+    type: "Workshop",
+  },
+  17: {
+    purpose: "Workshop - cloud-computing",
+    amount: 199.0,
+    type: "Workshop",
+  },
+  18: {
+    purpose: "Workshop - ar-vr",
+    amount: 149.0,
+    type: "Workshop",
+  },
+};
+
+exports.purposeConfig = {
+  "Workshop - stock-o-nomics": {
+    eventId: 16,
+    type: "Workshop",
+  },
+  "Workshop - cloud-computing": {
+    eventId: 17,
+    type: "Workshop",
+  },
+  "Workshop - ar-vr": {
+    eventId: 18,
+    type: "Workshop",
+  },
+};
